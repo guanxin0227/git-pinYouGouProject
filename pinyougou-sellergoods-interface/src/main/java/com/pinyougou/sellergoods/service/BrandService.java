f00ba80dd65bc5a38b2139c049842dc5d243c820
GUANX <CST123456>
2020-02-05 14:57:01 +0800

    1.初始化搭建品优购商城 2.品牌列表及增删改查

---
+++ pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -0,0 +1,46 @@
+package com.pinyougou.sellergoods.service;
+
+import com.github.pagehelper.PageInfo;
+import com.pinyougou.model.Brand;
+
+import java.util.List;
+
+/**
+ *  @author: guanx
+ *  @Date: 2020/2/5 14:47
+ *  @Description:
+ */
+public interface BrandService {
+
+    /***
+     * 查询所有品牌
+     * @return
+     */
+    List<Brand> getAll();
+
+    /****
+     * 分页查询
+     * @param page
+     * @param size
+     * @return
+     */
+    PageInfo<Brand> getAll(int page, int size,Brand brand);
+
+    /***
+     * 增加品牌
+     * @param brand
+     * @return
+     */
+    int add(Brand brand);
+
+    /***
+     * 根据ID查询品牌
+     * @param id
+     * @return
+     */
+    Brand getById(Long id);
+
+    int updateById(Brand brand);
+
+    int deleteByIds(List<Long> ids);
+}
